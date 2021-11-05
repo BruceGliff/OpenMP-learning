f1ecb80b51189dce948a860bf201c942b051d3b1
BruceGliff <ilyaawaA@yandex.ru>
2021-11-05 10:15:46 -0400

    calculating e^x

--- task5/main.c
+++ task5/main.c
@@ -7,17 +7,43 @@
 #define PI 3.14159265358979323846264338327950288419716939
 
 double run(unsigned long long n);
+double run_e(unsigned long long n, double x);
+
+double factorial(int a) {
+    double res = 1.0;
+    for (int i = 2; i <= a; ++i)
+        res *= i;
+    return res;
+}
 
 int main (int argc, char *argv[]) {
   if (argc < 2) {
     printf("N does not stated!\n");
     return -1;
   }
+  double x = 2.5;
   unsigned long long const N = atoll(argv[1]);
+  double const e = run_e(N, x);
+  printf("Calculated: e^%g = %g\n", x, e);
   double const pi = run(N);
   printf("Calculated: %g\nError: %g\n", pi, fabs(PI - pi));
 
   return 0;
+}
+
+
+double run_e(unsigned long long n, double x) {
+  double res = 0;
+  // calculationg of e in power if x.
+#pragma omp parallel
+#pragma omp for reduction (+: res)
+  for (int i = 0; i <= n; ++i) {
+    double const fac = factorial(i);
+    // overcalculating of double.
+    double const add = i > 774 ? 0.0 : pow(x, i) / fac;
+    res += add;
+  }
+  return res;
 }
 
 double run(unsigned long long n) {
